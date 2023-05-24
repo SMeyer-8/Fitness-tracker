e9c2af47931fde0c8e66b553eb14ff088c615ef2
SMeyer-8 <sarah.blue.meyer@gmail.com>
2023-05-24 10:48:51 -0500

    second commit

--- db/seed.js
+++ db/seed.js
@@ -1,28 +1,34 @@
 const client = require("./client");
+const {
+  users,
+  activities,
+  routines,
+  routine_activities,
+} = require("./seedData");
 
-async function dropTables(){
-    console.log("Dropping tables...");
+async function dropTables() {
+  // Drop all tables in order
 }
 
-async function createTables(){
-    console.log("Creating tables...");
+async function createTables() {
+  // Define your tables and fields
 }
 
-async function populateTables(){
-    console.log("Populating tables...");
+async function populateTables() {
+  // Seed tables with dummy data from seedData.js
 }
 
-async function rebuildDb(){
-    client.connect();
-    try {
-        await dropTables();
-        await createTables();
-        await populateTables();
-    } catch (error) {
-        console.error(error);
-    } finally {
-        client.end();
-    }
+async function rebuildDb() {
+  client.connect();
+  try {
+    await dropTables();
+    await createTables();
+    await populateTables();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
 }
 
 rebuildDb();

670571f3ef7f2b68d426446bf5d31ad2e5d7b552
Elliot016 <elliot.carmona016@gmail.com>
2023-06-01 11:13:03 -0500

    Added to routes/ users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,36 @@
 const usersRouter = require("express").Router();
-const { getAllUsers } = require("../db/adapters/users");
+const jwt = require("jsonwebtoken");
+const {
+  getAllUsers,
+  getUserByUsername,
+  createUser,
+} = require("../db/adapters/users");
 
-//GET request to /api/users/
+//POST request to /api/users/register
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const _users = await getUserByUsername(username);
+    if (_user) {
+      next({
+        name: "ALREADY A USER",
+        message: "Registration successful",
+      });
+    }
+
+    // POST / users/login
+    const user = await createUser(username, password);
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "2w",
+    });
+    res.send({
+      message: "The username already exists",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
 usersRouter.get("/", async (req, res, next) => {
   try {
     const users = await getAllUsers();
@@ -9,7 +38,6 @@ usersRouter.get("/", async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-  res.send("USERS COMING SOON TO A PLACE NEAR YOU!");
 });
 
 module.exports = usersRouter;

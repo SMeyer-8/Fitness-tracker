4f0b4ac9f99cf6a49e6df69a95a79114df4ccf58
Elliot016 <elliot.carmona016@gmail.com>
2023-06-05 10:08:05 -0500

    Added cookies to the Auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,73 @@
 const authRouter = require('express').Router();
 const bcrypt = require("bcrypt");
 const SALT_ROUNDS = 10;
-const { createUser } = require("../db/adapters/users");
+const { createUser, getUserByUsername } = require("../db/adapters/users");
 
 //signup
 authRouter.post("/signup", async (req, res, next) => {
-    try {
-        const { username, password } = req.body;
-        const hashedPassword = bcrypt.hash(password, SALT_ROUNDS)
-        const user = await createUser({ username, password: hashedPassword });
-        res.send(user);
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = bcrypt.hash(password, SALT_ROUNDS);
+    const user = await createUser({ username, password: hashedPassword });
+    delete user.password;
+
+    const token = jwt.sign(user, process.env.JWT_SECRET);
+    console.log("token:", token);
+
+    res.cookie("token", token, {
+      sameSite: "strict",
+      httpOnly: true,
+      signed: true,
+    });
+
+    res.send({ user, token });
   } catch (error) {
     next(error);
   }
 });
+
+authRouter.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await getUserByUsername(username);
+    const checkedPassword = await bcrypt.compare(password, user.password);
+    if (checkedPassword) {
+      const token = jwt.sign(user, process.env.JWT_SECRET);
+
+      res.cookie("token", token, {
+        sameSite: "strict",
+        httpOnly: true,
+        signed: true,
+      });
+      res.send({ message: "You're logged in!" });
+    }
+  } catch (error) {
+    next({
+      name: "Login FAiled",
+      message: "Invalid username or password",
+    });
+  }
+});
+
+authRouter.get("/logout", async (req, res, next) => {
+  try {
+    res.clearCookie("token", {
+      sameSite: "strict",
+      httpOnly: true,
+      signed: true,
+    });
+    res.send({
+      loggedIn: false,
+      message: "You're logged out!",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+      authRouter.get("/me", authRequired, (req, res, next) => {
+        res.send(req.user);
+      });
+  
 
 module.exports = authRouter;

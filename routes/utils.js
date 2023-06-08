3d48c3b1afd1666003baa245d18b1d01bd86d403
SMeyer-8 <sarah.blue.meyer@gmail.com>
2023-06-08 10:25:40 -0500

    added auth getUserByUserName, cookieParser, .env, auth.js token, utils.js, JWt token

---
+++ routes/utils.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+const authRequired = (req, res, next) => {
+  try {
+    const token = req.signedCookies.token;
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Req.user: ", req.user);
+  } catch (error) {
+    res.status(401).send({
+      success: false,
+      message: "You are not authorized!",
+    });
+    return;
+  }
+  next();
+};
+
+module.exports = { authRequired };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 const server = express();
 
@@ -17,12 +18,19 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 server.use(express.static(path.join(__dirname, "./client")));
+server.use(morgan("dev"));
 server.use(cors());
+server.use(cookieParser(process.env.COOKIE_SECRET));
 
 // Routes
 app.use("/api", require("./routes"));
 
-server.use((req, res, next) => {
+const { authRequired } = require("./routes/utils");
+server.get("/test", authRequired, (req, res, next) => {
+  res.send("You are authorized!");
+});
+
+server.use((err, req, res, next) => {
   res.sendFile(path.join(__dirname, "./client/dist", "index.html"));
 });
 
